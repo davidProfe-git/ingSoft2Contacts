eba60893b0705450587a516db9f97f2c82469614
Erik <calderonerik2004@gmail.com>
2025-09-25 01:43:43 -0500

    Consumo de la api

---
+++ Scritp.js
@@ -0,0 +1,20 @@
+fetch("https://randomuser.me/api/?results=5&nat=mx")
+      .then(resp => resp.json())
+      .then(datos => {
+        const tarjeta = document.getElementById("tarjeta1")
+        datos.results.forEach(contactos => {
+          const contenido = document.createElement("div")
+          contenido.className = "card"
+          contenido.innerHTML = `
+            <img src="${contactos.picture.large}" alt="Foto de ${contactos.name.first}" />
+            <h2>${contactos.name.first} ${contactos.name.last}</h2>
+            <p>📞 ${contactos.phone}</p>
+            <p>📧 ${contactos.email}</p>
+            <p>⚧ ${contactos.gender}</p>
+            <p>🌍 ${contactos.location.country}</p>
+            <p>🕒 Edad: ${contactos.dob.age}</p>
+          `
+          tarjeta.appendChild(contenido)
+        })
+      })
+      .catch(error => console.error("Error:", error))
