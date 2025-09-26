1bdf6c3d823291d13d33ff3d206d6d69e6f87c2b
S4NT14GOR3Y3S <reyesvanegassantiago4@gmail.com>
2025-09-26 18:06:55 -0500

    cambioss

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const btnLoad = document.getElementById("btnLoad");
 const userContainer = document.getElementById("userContainer");
 
-// Función para traer un usuario aleatorio
+
 async function fetchUser() {
   try {
     const response = await fetch("https://randomuser.me/api/");
     const data = await response.json();
     const user = data.results[0];
 
-    // Crear card
     const card = document.createElement("div");
     card.classList.add("card");
 
@@ -27,8 +26,8 @@ async function fetchUser() {
   }
 }
 
-// Cargar un usuario inicial
+
 fetchUser();
 
-// Cargar más al dar clic
+
 btnLoad.addEventListener("click", fetchUser);
